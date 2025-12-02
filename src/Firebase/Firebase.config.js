6efd08abd44b942fc85f6c11d54dad5378e5b846
mrchipsmaruf <mrchipsmaruf@gmail.com>
2025-12-02 20:47:27 +0600

    first commit

--- src/Firebase/Firebase.config.js
+++ src/Firebase/Firebase.config.js
@@ -9,7 +9,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyCyko4HZw-3uu9owa6bBN3il2WkgGayheI",
   authDomain: "warm-paws-4ddaf.firebaseapp.com",
   projectId: "warm-paws-4ddaf",
-  storageBucket: "warm-paws-4ddaf.firebasestorage.app",
+  storageBucket: "warm-paws-4ddaf.appspot.com",
   messagingSenderId: "108745928535",
   appId: "1:108745928535:web:627ab7fae2c09033df1812",
   measurementId: "G-P77Q84WNRR"
